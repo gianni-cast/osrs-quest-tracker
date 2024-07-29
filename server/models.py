fc4e4537f595007733f52fe709c0ac37186c6b5d
Gianni Castellano <gianniccastellano@gmail.com>
2024-07-29 17:43:19 -0400

    complete player and quest models

--- server/models.py
+++ server/models.py
@@ -3,4 +3,27 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-# Models go here!
+class Quest(db.Model, SerializerMixin):
+    __tablename__ = "quests"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    description = db.Column(db.Text, nullable=False)
+
+    player_quests = db.relationship('PlayerQuest', back_populates='quest')
+
+    serialize_rules = ('-player_quests.quest', '-player_quests.player')
+
+class Player():
+    __tablename__ = "players"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    level = db.Column(db.Integer, nullable=False)
+
+    player_quests = db.relationship('PlayerQuest', back_populates='player')
+
+    serialize_rules = ('-player_quests.player', '-player_quests.quest')
+
+class PlayerQuest():
+    __tablename__ = "player_quests"
