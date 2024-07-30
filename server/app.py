9b68f8917e97c5fbffdc6447725789399c8cc875
Gianni Castellano <gianniccastellano@gmail.com>
2024-07-30 13:52:25 -0400

    made some routes with get methods

--- server/app.py
+++ server/app.py
@@ -3,13 +3,13 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
 # Add your model imports
-
+from models import Quest, Player, PlayerQuest
 
 # Views go here!
 
@@ -17,6 +17,51 @@ from config import app, db, api
 def index():
     return '<h1>Project Server</h1>'
 
+@app.route('/players', methods=["GET"])
+def players_route():
+    players = []
+    for player in Player.query.all():
+        player_dict = {
+            "level": player.level,
+            "id": player.id,
+            "name": player.name,
+        }
+        players.append(player_dict)
+    
+    response = make_response(
+        players,
+        200,
+        {"Content-Type": "application/json"}
+    )
+
+    return response
+
+@app.route('/players/<int:id>', methods=["GET"])
+def get_player(id):
+    player = Player.query.get(id)
+    if player:
+        return make_response(player.to_dict(), 200)
+    else:
+        return make_response({"error": "Player does not exist"}, 404)
+
+@app.route('/quests', methods=["GET"])
+def quests_route():
+    quests = []
+    for quest in Quest.query.all():
+        quest_dict = {
+            "description": quest.description,
+            "id": quest.id,
+            "name": quest.name,
+        }
+        quests.append(quest_dict)
+    
+    response = make_response(
+        quests,
+        200,
+        {"Content-Type": "application/json"}
+    )
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
 
 from config import db
 
@@ -14,7 +15,10 @@ class Quest(db.Model, SerializerMixin):
 
     serialize_rules = ('-player_quests.quest', '-player_quests.player')
 
-class Player():
+    def __repr__(self):
+        return f"<Quest {self.name}, {self.description}>"
+
+class Player(db.Model, SerializerMixin):
     __tablename__ = "players"
 
     id = db.Column(db.Integer, primary_key=True)
@@ -25,5 +29,33 @@ class Player():
 
     serialize_rules = ('-player_quests.player', '-player_quests.quest')
 
-class PlayerQuest():
+    @validates('level')
+    def validate_level(self, key, value):
+        if not (3 <= value <= 126):
+            raise ValueError("Player Level must be between 3 and 126")
+        return value
+    
+    def __repr__(self):
+        return f"<Player {self.name}, {self.level}>"
+
+class PlayerQuest(db.Model, SerializerMixin):
     __tablename__ = "player_quests"
+
+    id = db.Column(db.Integer, primary_key=True)
+    progress = db.Column(db.String(20), nullable=False)
+    player_id = db.Column(db.Integer, db.ForeignKey('players.id'), nullable=False)
+    quest_id = db.Column(db.Integer, db.ForeignKey('quests.id'), nullable=False)
+
+    player = db.relationship('Player', back_populates='player_quests')
+    quest = db.relationship('Quest', back_populates='player_quests')
+    
+    serialize_rules = ('-player', '-quest')
+
+    @validates('progress')
+    def validate_progress(self, key, value):
+        if value not in ['Not Started', 'In Progress', 'Completed']:
+            raise ValueError("Progress must be: 'Not Started', 'In Progress', or 'Completed'")
+        return value 
+    
+    def __repr__(self):
+        return f"<PlayerQuest {self.progress}>"
